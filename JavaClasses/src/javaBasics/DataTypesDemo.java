eab2168783a0a1e85e5720f1870ff8b207447c11
karimunnisa12345 <arshiyajyothi@gmail.com>
2023-02-24 12:48:41 +0530

    commiting java datatype code

---
+++ JavaClasses/src/javaBasics/DataTypesDemo.java
@@ -0,0 +1,28 @@
+package javaBasics;
+
+public class DataTypesDemo {
+	//variable initialization
+	byte b=10;
+	short sh=20;
+	int i=30;
+	long ln=40l;
+	float f1=50.54f;
+	double d1=120.25d;
+	char ch='a';
+	boolean bo=true;
+	
+	public static void main(String[] args) {
+		
+		DataTypesDemo obj=new DataTypesDemo();
+		System.out.println(obj.b);
+		System.out.println(obj.sh);
+		System.out.println(obj.i);
+		System.out.println(obj.ln);
+		System.out.println(obj.f1);
+		System.out.println(obj.d1);
+		System.out.println(obj.ch);
+		System.out.println(obj.bo);
+		
+	}
+
+}
